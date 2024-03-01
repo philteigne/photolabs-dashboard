093252c9f00592483a74c64d75b865f2b972fddd
Phil <philippe.teigne@gmail.com>
2024-02-29 22:03:06 -0500

    create and build functionality of panels component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,20 +29,40 @@ const data = [
 
 class Dashboard extends Component {
   
-  state = { loading: true };
-
+  state = {
+    loading: false,
+    focused: null,
+  };
+  
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused ? null : id
+    }))
+  } 
+  
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
+    
     if (this.state.loading) {
       return <Loading />;
     }
 
-    this.data.map((panel) => {
-      return <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value}/>
-    })
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map(panel => (
+      <Panel
+        key={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={event => {
+          this.selectPanel(panel.id)
+        }}
+      />
+    ));
 
-    return <main className={dashboardClasses} />;
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,19 +2,15 @@ import React, { Component } from 'react'
 
 class Panel extends Component {
   render() {
+    const { label, value, onSelect } = this.props;
 
-    const { label, value } = this.props;
-    
-    return(
-      <section
-        className="dashboard__panel"
-      >
-        <h1 className="dashboard__panel-header">{label}</h1>
-        <p className="dashboard__panel-value">{value}</p>
-      </section>
+    return (
+    <section className="dashboard__panel" onClick={onSelect}>
+      <h1 className="dashboard__panel-header">{label}</h1>
+      <p className="dashboard__panel-value">{value}</p>
+    </section>
     );
   }
-
 }
 
 export default Panel;
